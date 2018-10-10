1442fd36ae969341e0ffab363624134ecfbed6e8
CAH1983 <carolineho1983@yahoo.fr>
2018-10-10 11:18:18 +0100

    created 2 testing files

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,6 @@
 const port = process.env.PORT || 4000;
-const dbURI = 'mongodb://localhost:27017/project-03';
+const env = process.env.NODE_ENV || 'dev';
+const dbURI = `mongodb://localhost:27017/project-03-${env}`;
 const secret = 'shhh';
 
 module.exports = { port, dbURI, secret };
